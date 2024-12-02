b1d3bdcd80d2f7732bcb2b7239aa2675ccbf2111
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-12-02 23:30:19 +0900

    122024

--- signUp/index.php
+++ signUp/index.php
@@ -1,3 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
 
-echo "hello world";
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+    <div id="bodyForm">
+            <form action="php/signup.php" method="post">
+                <label for="username">Username:</label>
+                    <input type="text" name="username" placeholder="Enter Username">
+                <label for="password">Password:</label>
+                    <input type="password" name="password" placeholder="Enter Password">
+                <label for="email">Email:</label>
+                    <input type="email" name="email" placeholder="Enter Email">
+
+                <button type="submit" class="btnSubmit">Submit</button>
+            </form>
+    </div>
+</body>
+</html>
---
+++ signUp/php/signup.php
@@ -0,0 +1,30 @@
+<?php
+
+    // Database connection
+$host = '127.0.0.1';
+$port = '8889';
+$dbname = 'db_users';
+$username = 'root';
+$password = 'root';
+
+try {
+    $pdo = new PDO ("mysql:host=$host;port=$port;dbname=$dbname; $username, $password");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    var_dump($pdo);
+    exit;
+
+    $sql = "INSERT INTO users (username, password, email) VALUES (:username, :password, :email) ";
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        ':username' => $_POST['username'], // users name
+        ':password' => password_hash($_POST['password'], PASSWORD_BCRYPT), // users password
+        ':email' => $_POST['email'], // users email
+    ]);
+
+    echo "Signup Sucessful!"; // Output a success message upon insertion into database
+    
+} catch (PDOException $e) {
+    echo "Sign up error: " . $e->getMessage();
+}

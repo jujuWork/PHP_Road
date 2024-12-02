35c6ec01bb9193947fc93387bedb9c9c1c07bc13
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-12-02 21:46:05 +0900

    120224

---
+++ signUp/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    
+    <form action="signup.php" method="post">
+        <label for="username">Username:</label>
+            <input type="text" name="username" placeholder="Enter Username">
+        <label for="password">Password:</label>
+            <input type="password" name="password" placeholder="Enter Password">
+        <label for="email">Email:</label>
+            <input type="email" name="email" placeholder="Enter Email">
+
+        <button type="submit">Submit</button>
+    </form>
+
+</body>
+</html>

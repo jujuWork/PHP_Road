f64852b0d51dd3257d8e7b4e3ae7eee8f45aa4bb
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-12-04 22:17:58 +0900

    120424

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/login_styles.css">
 </head>
 <body>
         <?php if(!empty($message)): ?>
@@ -19,13 +19,21 @@
     </header>
 
     <main id="bodyForm">
-            <form action="signUp/php/login_logic.php" method="post">
+            <form action="php/login_logic.php" method="post">
                 
-                <input type="text" name="username" placeholder="Enter Username">
-                <input type="password" name="password" placeholder="Enter Password">
-              
+                <input type="text" name="username" placeholder="    Username">
+                <input type="password" name="password" placeholder="    Password">
+                    <p class="forgot">Forgot <a href="forgot.php">Username / Password?</a></p>
                 <button type="submit" class="btnSubmit">Login</button>
             </form>
+        <div class="signUpFooter">
+            <p>Don't have an account?</p>
+            <a href="index.php" class="noAccount"><p>Sign Up Now</p></a>
+        </div>
     </main>
+
+    <footer>
+        
+    </footer>
 </body>
 </html>

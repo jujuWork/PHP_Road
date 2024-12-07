f511f799e5065ab0456fcb0b7831f5748420abfb
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-12-07 22:36:04 +0900

    120724

---
+++ profile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>
+</head>
+<body>
+    
+    <header>
+        <h1>Welcome to your Profile</h1>
+    </header>
+
+    <main>
+        <section class="profile-info">
+            <h2>hello</h2>
+            <p>
+                <strong></strong>
+            </p>
+        </section>
+    </main>
+
+</body>
+</html>

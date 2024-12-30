4802db379820edb1b7ebb3321b45d264271f8474
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-12-30 22:29:03 +0900

    123024

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Portfolio</title>
+
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    Portfolio
+    <div class="leftSide">
+
+    </div>
+    <div class="rightSide">
+
+    </div>
 </body>
 </html>

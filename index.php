8ad314ec23d23a6a7af1d58fea9379bb035060d0
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2024-12-31 00:18:15 +0900

    123024

--- index.php
+++ index.php
@@ -6,14 +6,21 @@
     <title>Portfolio</title>
 
     <link rel="stylesheet" href="css/index.css">
+
+            <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="leftSide">
         <header>
-            <img src="upload/images/profilepic.jpg" alt="profile" width="120px">
-            <span>
-                <h1>Sanchez Joharry</h1>
-            </span>
+            <div class="profile_info">
+                <img src="upload/images/profilepic.jpg" alt="profile" width="150px">
+                <span>
+                    <h1>Sanchez <br> Joharry</h1>
+                </span>
+            </div>
         </header>
     </div>
     <div class="rightSide">

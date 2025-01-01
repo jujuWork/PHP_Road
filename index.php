fd4912683fc36127be6999a554fb1706f330b8c5
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-01-01 14:37:06 +0900

    010125

--- index.php
+++ index.php
@@ -17,9 +17,56 @@
         <header>
             <div class="profile_info">
                 <img src="upload/images/profilepic.jpg" alt="profile" width="150px">
-                <span>
-                    <h1>Sanchez <br> Joharry</h1>
+                    <span>
+                        <h1>Sanchez <br> Joharry</h1>
+                    </span>
+            </div>
+            <div class="contact">
+                <table>
+                    <tr>
+                        <td><img src="upload/images/CS.png" alt="contact"></td>
+                        <td>CONTACT ME</td>
+                    </tr>
+                    <tr>
+                        <td><img src="upload/images/email.png" alt="email"></td>
+                        <td>Email:</td>
+                    </tr>
+                    <tr>
+                        <td><img src="upload/images/mobile.png" alt="mobile"></td>
+                        <td>090-9923-3642</td>
+                    </tr>
+                    <tr>
+                        <td><img src="upload/images/linkedin.png" alt="linkedIN"></td>
+                        <td>linkedIN</td>
+                    </tr>
+                    <tr>
+                        <td><img src="upload/images/location.png" alt="location"></td>
+                        <td>Location:</td>
+                    </tr>
+                </table>
+            
+            
+            
+            
+            
+            
+                <!-- <span>
+                    <h2>
+                        <p><img src="upload/images/contact.png" alt="contact"> CONTACT ME</p>
+                    </h2>
                 </span>
+                <span>
+                    <p>090-9923-3642</p>
+                </span>
+                <span>
+                    <p>joharrywork@gmail.com</p>
+                </span>
+                <span>
+                    <p>linkedin</p>
+                </span>
+                <span>
+                    <p>location</p>
+                </span> -->
             </div>
         </header>
     </div>

aea49aed229311ec675c890127586510a40236fc
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-02-10 22:44:33 +0900

    021025

---
+++ javascript/index_html.js
@@ -0,0 +1,22 @@
+////////////// PROGRESS BAR ////////////
+    // Skill levels
+const skillLevels = {
+    html: 90,   // 90%
+    css: 85,    // 85%
+    php: 60,    // 60%%
+    mysql: 60   // 60%
+};
+    // Function to update progress bar dynamically
+function updateProgressBar(skill, value) {
+    let bar = document.getElementById(skill + "_bar");
+    bar.style.width = value + "%";
+    bar.textContent = value + "%";
+}
+
+    //Animate progress on pageload
+window.onload = function () {
+    updateProgressBar("html", skillLevels.html);
+    updateProgressBar("css", skillLevels.css);
+    updateProgressBar("php", skillLevels.php);
+    updateProgressBar("mysql", skillLevels.mysql);
+};

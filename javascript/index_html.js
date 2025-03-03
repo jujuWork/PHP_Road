4c6c15f6b1275d53ad792e056f0a75a04c4358c6
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-03-03 23:03:55 +0900

    030325

--- javascript/index_html.js
+++ javascript/index_html.js
@@ -1,11 +1,15 @@
 ////////////// PROGRESS BAR ////////////
     // Skill levels
 const skillLevels = {
-    html: 90,   // 90%
-    css: 85,    // 85%
-    js: 10, // 10%
-    php: 60,    // 60%
-    mysql: 70   // 70%
+    html: 90,           // 90%
+    css: 85,            // 85%
+    js: 10,             // 10%
+    php: 60,            // 60%
+    mysql: 70,          // 70%
+    bisaya: 99,         // 99%
+    tagalog: 90,        // 80%
+    english: 70,        // 60%
+    japanese: 30        // 60%
 };
     // Function to update progress bar dynamically
 function updateProgressBar(skill, value) {
@@ -21,4 +25,8 @@ window.onload = function () {
     updateProgressBar("js", skillLevels.js);
     updateProgressBar("php", skillLevels.php);
     updateProgressBar("mysql", skillLevels.mysql);
+    updateProgressBar("bisaya", skillLevels.bisaya);
+    updateProgressBar("tagalog", skillLevels.tagalog);
+    updateProgressBar("english", skillLevels.english);
+    updateProgressBar("japanese", skillLevels.japanese);
 };
